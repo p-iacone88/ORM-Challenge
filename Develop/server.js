e1aef3cf07513f3f6d3d249e01052a31a1719474
[philip iacone] <[philiacone@gmail.com]>
2024-01-09 17:44:55 -0500

    Added comments on server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const routes = require('./routes');
+// Require packages and modules
+const express = require('express'); // import Express.js
+const routes = require('./routes'); // Imports routes
 // import sequelize connection
 const sequelize = require('./config/connection');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express(); // create instance of Express.js
+const PORT = process.env.PORT || 3001; // Define port to listen on 3001
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Middleware to parse JSON data
+app.use(express.urlencoded({ extended: true })); // Middleward for parsing URL encoded data
 
-app.use(routes);
+app.use(routes); // use defined routes
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`)
+  app.listen(PORT, () => { // start server and listen on 3001
+    console.log(`App listening on port ${PORT}!`) // log message to show server is running and listening
   })
 });
